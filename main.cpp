9c2047c9bce1155da2fe76dda7cccdfa71654009
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2019-08-26 00:03:47 -0500

    WARNING
    Para que resulta, se debe de dar dos veces al entar al momento de submitear el input.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+
+void convertir(vector<string> texto)
+{
+    bool variable = false;
+    bool cambiado = false;
+    for(int i = 0; i<texto.size();i++)
+    {
+        for(int j = 0; j<texto[i].size();j++)
+        {
+            if(texto[i][j]=='\"')
+            {
+                if(!variable)
+                {
+                    texto[i][j] = '`';
+                    cout<<texto[i][j];
+                    cout<<texto[i][j];
+                }
+                else
+                {
+                    texto[i][j] = '\'';
+                    cout<<texto[i][j];
+                    cout<<texto[i][j];
+                }
+                variable = !variable;
+                cambiado = !cambiado;
+
+            }else{
+
+                cout<<texto[i][j];
+            }
+
+        }
+        if(i != texto.size()-1){
+            cout<<"\n";
+            
+        }
+    }
+}
+
+int main() {
+    vector<string> input;
+    string texto = "aa";
+    while(texto!=""){
+        getline(cin, texto);
+        input.push_back(texto);
+    }
+    convertir(input);
+
     return 0;
 }
